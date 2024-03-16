6225f7fe6077eeb9d71c203bd3e1344b399c2c44
MoonBasedKas <kassidy.maberry@gmail.com>
2024-03-15 19:11:29 -0600

    Syncing

--- func.py
+++ func.py
@@ -1,5 +1,22 @@
-def test(x):
-    print(x)
+class x:
+    def __init__(self) -> None:
+        self.q = 2
+    
+    def add(self, x: float, y: float):
+        return x + y + 2
+    
 
-h = test 
-h(2)
+class z:
+    def __init__(self) -> None:
+        self.q = 3
+
+class y (x, z):
+    def __init__(self) -> None:
+        self.x.__init__()
+        self.y.__init__()
+        self.q = 1
+        print(self.super().q)
+    
+
+m = y()
+# print(z.add(2, 2))
