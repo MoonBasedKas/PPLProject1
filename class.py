1a6d90ac462af60699949d957b63d1d86c935ad8
MoonBasedKas <kassidy.maberry@gmail.com>
2024-03-16 23:56:08 -0600

    Wrote some more

--- class.py
+++ class.py
@@ -1,10 +1,33 @@
-class test:
-    __x = []
-
+# Inheritance in Python.
+class a:
     def __init__(self) -> None:
-        self.__x.append(1)
-        print(self.__x)
+        self.x = 3
+        return 3
+    
+    def p(self):
+        print("A")
 
-z = []
-for x in range(5):
-    z.append(test())
+
+class b:
+    def __init__(self) -> None:
+        self.x = 1
+        self.y = 2
+        return 2
+
+    def p(self):
+        print("B")
+
+    def bfunc(self):
+        self.z = 0
+        return 4
+
+class ab (a,b):
+    def __init__(self) -> None:
+        print(super().__init__())
+        print(self.x)
+        print(self.bfunc(), self.z)
+        self.p()
+
+        
+
+z = ab()
